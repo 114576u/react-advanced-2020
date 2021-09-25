52fd23ca200acc0eed833d22d20549bb1867557d
114576u <114576u@gmail.com>
2021-09-25 17:43:16 +0200

    exercises, 4:41:25

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/3-useState-array';
+import Setup from './tutorial/1-useState/setup/5-useState-counter';
 
 function App() {
   return (
     <div className='container'>
       <h2>this is an advanced react tutorial</h2>
+      <h2>---------------</h2>
       <Setup />
     </div>
   );
